50d1c82f60738146ca432a3212b18c9d96bceb24
krlee354 <kangrim1025@gmail.com>
2023-01-23 10:21:50 +0900

    1/23

--- Assets/Scripts/Extension.cs
+++ Assets/Scripts/Extension.cs
@@ -6,16 +6,23 @@ using UnityEngine.EventSystems; // PointerEventData 사용하기 위함
 
 public static class Extension // Extension Method 만들기 // Extension class를 만들때는 static이 필요, MonoBehaviour은 필요없음
 {
-    public static void AddUIEvent(this GameObject go, Action<PointerEventData> action, Define.UIEvent type = Define.UIEvent.Click) // UI Event를 추가해주는 함수 정의 // (GameObject, 구독시킬 함수(CallBack), 구독을 받을 이벤트(기본은 클릭으로 설정))
+    public static T GetOrAddComponent<T>(this GameObject go) where T : UnityEngine.Component // 해당 Component가 있으면 받아오고, 없으면 추가하라는 함수 // 조건을 입력해줘야 AddComponent<T>에 오류 발생하지 않음
     {
+        /* 사용방법 */
+        //go.GetOrAddComponent<T>(go);
         // Extention 메소드가 그냥 static 메소드가 아닌 확장 메서드로 인식되는 이유는 첫번째 파라미터에 this가 붙기 때문이다.
         // 여기서 this는 2가지 역할을 한다, 
         // 1. 어느 클래스의 확장 메서드가 될지 결정한다.
         // 2. 해당되는 클래스의 객체를 불러온다.
 
+        return Util.GetOrAddComponent<T>(go);
+    }
 
-        //go.AddUIEvent(~~~); // 다른 곳에서 이렇게 사용 가능해짐
+    public static void BindEvent(this GameObject go, Action<PointerEventData> action, Define.UIEvent type = Define.UIEvent.Click) // UI Event를 추가해주는 함수 정의 // (GameObject, 구독시킬 함수(CallBack), 구독을 받을 이벤트(기본은 클릭으로 설정))
+    {
+        /* 사용방법 */
+        //go.BindEvent(action);
 
-        UI_Base.AddUIEvent(go, action, type);
+        UI_Base.BindEvent(go, action, type);
     }
 }
--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 
 public class ResourceManager
 {
-    public T Load<T>(string path) where T : Object // 일반화를 통해 형식생성
+    public T Load<T>(string path) where T : Object // 경로에서 Object를 불러와서 넣기 // 일반화 // T는 Object면 된다는 조건 // 단순 랩핑
     {
         return Resources.Load<T>(path);
     }
 
-    public GameObject Instantiate(string path, Transform parent = null) // 오브젝트 생성
+    public GameObject Instantiate(string path, Transform parent = null) // Instance 생성 메소드 // 메소드 랩핑
     {
         GameObject prefab = Load<GameObject>($"Prefabs/{path}");
-        if (prefab == null)
+        if (prefab == null) // prefab을 찾을 수 없었다면
         {
             Debug.Log($"Failed to load prefab : {path}");
-            return null;
+            return null; // null 반환
         }
 
-        return Object.Instantiate(prefab, parent);
+        GameObject go = Object.Instantiate(prefab, parent);
+        int index = go.name.IndexOf("(Clone)"); // (Clone) 문자열이 있는지 찾고, 그 인덱스를 반환
+        if (index > 0) // (Clone) 문자열이 있었다면
+        {
+            go.name = go.name.Substring(0, index); // 0번부터 index 전까지 잘라서 이름을 저장
+        }
+
+        return go;
     }
 
-    public void Destroy(GameObject go, float f) // 오브젝트 소멸
+    public void Destroy(GameObject go, float f = 0.0f) // Object 삭제 메소드
     {
-        if (go == null)
+        if (go == null) // 없는 Object였다면
         {
-            return;
+            return; // 그냥 return
         }
 
         Object.Destroy(go, f);
